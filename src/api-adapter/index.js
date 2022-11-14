8a9d7199856c600d3e1a3f120580a61e8a76bc5e
timothy <timothybrennan8@gmail.com>
2022-11-14 14:05:51 -0500

    setup the files and folders that we will need. Just setup the project.

---
+++ src/api-adapter/index.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://fitnesstrac-kr.herokuapp.com"
+
+export async function registerUser(username, password) {
+
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 import {Main} from './components'
 
-
-
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
 
